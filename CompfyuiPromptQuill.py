5a7173d45c8ed0e72b121a889cb9412d02e51106
Werner Oswald <osiworx@gmail.com>
2024-05-05 11:23:52 +0200

    first comfyui node

--- CompfyuiPromptQuill.py
+++ CompfyuiPromptQuill.py
@@ -13,80 +13,153 @@
 # permissions and limitations under the License.
 
 import requests
-from PIL import Image
-import numpy as np
-import base64
-from io import BytesIO
-class Client:
-    default_url = "http://127.0.0.1:64738"
-    def __init__(self, url=None):
-        if url is None:
-            # Use class variable if no argument provided
-            url = Client.default_url
-        self.url = url
-        self.generate_url = f'{self.url}/get_prompt'
-
-
-
-    def generate(self, query, model):
-        # Prepare your JSON data
-        payload = {'query': query,
-                   'model':model}
-        # Set the Content-Type header to indicate JSON data
-        headers = {"Content-Type": "application/json"}
-        # Send the POST request
-        response = requests.post(self.generate_url, json=payload, headers=headers)
-        # Check for successful response
-        if response.status_code == 200:
-            # Access the response data (if any)
-            response_data = response.json()
-            return response_data
-        else:
-            return response.text
 
 default_url = "http://127.0.0.1:64738"
+
+
+class Client:
+	default_url = default_url
+
+	def __init__(self, url=None):
+		if url is None:
+			# Use class variable if no argument provided
+			url = Client.default_url
+		self.url = url
+		self.generate_url = f'{self.url}/get_prompt'
+		self.sail_url = f'{self.url}/get_next_prompt'
+
+
+	def send_api_call(self,payload, url):
+		# Set the Content-Type header to indicate JSON data
+		headers = {"Content-Type": "application/json"}
+		# Send the POST request
+		response = requests.post(url, json=payload, headers=headers)
+		# Check for successful response
+		if response.status_code == 200:
+			# Access the response data (if any)
+			response_data = response.json()
+			return response_data
+		else:
+			return response.text
+
+	def generate(self, query, model):
+		# Prepare your JSON data
+		payload = {'query': query,
+				   'model': model}
+		return self.send_api_call(payload, self.generate_url)
+
+
+	def sail(self, query, distance, summary, rephrase, rephrase_prompt, add_style, style, add_search, search, reset_journey):
+		payload = {'query': query,
+				   'distance': distance,
+				   'summary': summary,
+				   'rephrase': rephrase,
+				   'rephrase_prompt': rephrase_prompt,
+				   'add_style': add_style,
+				   'style': style,
+				   'add_search': add_search,
+				   'search': search,
+				   'reset_journey': reset_journey
+				   }
+		return self.send_api_call(payload, self.sail_url)
+
 class PromptQuillGenerate:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "prompt": ("STRING", {
-                    "multiline": True,
-                    "default": "A cute cat?"
-                }),
-                "url": ("STRING", {
-                    "multiline": False,
-                    "default": default_url
-                }),
-            },
-        }
+	@classmethod
+	def INPUT_TYPES(s):
+		return {
+			"required": {
+				"prompt": ("STRING", {
+					"multiline": True,
+					"default": "A cute cat?"
+				}),
+				"url": ("STRING", {
+					"multiline": False,
+					"default": default_url
+				}),
+			},
+		}
 
-    RETURN_TYPES = ("STRING","STRING",)
-    RETURN_NAMES = ("Prompt","NegativePrompt",)
+	RETURN_TYPES = ("STRING", "STRING",)
+	RETURN_NAMES = ("Prompt", "NegativePrompt",)
 
-    FUNCTION = "prompt_quill_generate"
-    CATEGORY = "PromptQuill"
+	FUNCTION = "prompt_quill_generate"
+	CATEGORY = "PromptQuill"
 
-    def prompt_quill_generate(self, prompt, url, model=None):
+	def prompt_quill_generate(self, prompt, url, model=None):
+		client = Client(url=url)
 
-        client = Client(url=url)
+		response = client.generate(model=model, query=prompt)
 
-        response = client.generate(model=model, query=prompt)
-
-        return (response['prompt'],response['neg_prompt'],)
-
+		return (response['prompt'], response['neg_prompt'],)
 
 
+class PromptQuillSail:
+	def __init__(self):
+		pass
+
+	@classmethod
+	def IS_CHANGED(cls, **kwargs):
+		return float("NaN")
+
+	@classmethod
+	def INPUT_TYPES(s):
+		return {
+			"required": {
+				"prompt": ("STRING", {
+					"multiline": True,
+					"default": "A cute cat?"
+				}),
+				"distance": ("INT", {"default": 20, "min": 1, "max": 10000, "step": 1}),
+				"summary": (("false", "true"), {"default": "false"}),
+				"rephrase": (("false", "true"), {"default": "false"}),
+				"rephrase_prompt": ("STRING", {
+					"multiline": True,
+					"default": ""
+				}),
+				"add_style": (("false", "true"), {"default": "false"}),
+				"style": ("STRING", {
+					"multiline": True,
+					"default": ""
+				}),
+				"add_search": (("false", "true"), {"default": "false"}),
+				"search": ("STRING", {
+					"multiline": True,
+					"default": ""
+				}),
+				"reset_journey": (("false", "true"), {"default": "false"}),
+				"url": ("STRING", {
+					"multiline": False,
+					"default": default_url
+				}),
+			},
+		}
+
+	RETURN_TYPES = ("STRING", "STRING",)
+	RETURN_NAMES = ("Prompt", "NegativePrompt",)
+
+	FUNCTION = "prompt_quill_sail"
+	CATEGORY = "PromptQuill"
+
+	def prompt_quill_sail(self, prompt, distance, summary, rephrase, rephrase_prompt, add_style, style, add_search,
+						  search, reset_journey, url):
+		client = Client(url=url)
+
+		response = client.sail(query=prompt, distance=distance, summary=summary, rephrase=rephrase,
+							   rephrase_prompt=rephrase_prompt, add_style=add_style, style=style, add_search=add_search,
+							   search=search, reset_journey=reset_journey)
+
+		return (response['prompt'], response['neg_prompt'],)
 
 
 NODE_CLASS_MAPPINGS = {
-    "PromptQuillGenerate": PromptQuillGenerate,
+	"PromptQuillGenerate": PromptQuillGenerate,
+	"PromptQuillSail": PromptQuillSail
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PromptQuillGenerate": "Prompt Quill Generate",
+	"PromptQuillGenerate": "Prompt Quill Generate",
+	"PromptQuillSail": "Prompt Quill Sailing"
 }
-
--- README.md
+++ README.md
@@ -24,4 +24,10 @@ use the [compfyui manager](https://github.com/ltdrdata/ComfyUI-Manager) "install
 
 A node that gives an ability to query Prompt Quill via given prompt. 
 
+### PromptQuillSailing
+
+A node that does some real magic, based on the vast ocean of data Prompt Quill uses you go on a journey along the data and you find amazing prompts.
+This is an exploration feature more than create a prompt for a fixed input. each time you call it, it will create a new prompt, based on the most far context from the last run.
+
+
 More nodes will follow soon
